ef7d00c5862450327b659567e1decf3d3738d336
Azhang9328 <azhang9328@gmail.com>
2020-01-23 14:16:40 -0800

    restrict users from others

--- app/controllers/favorite_clips_controller.rb
+++ app/controllers/favorite_clips_controller.rb
@@ -1,5 +1,6 @@
 class FavoriteClipsController < ApplicationController
     before_action :find_favorite_clip, only: [:show, :edit, :update, :destroy]
+    before_action :correct_user?, only: [:edit, :update, :destroy]
 
     def update
         @favorite_clip.update(params.require(:favorite_clip).permit(:title))
@@ -25,6 +26,16 @@ class FavoriteClipsController < ApplicationController
     end
 
     private 
+
+    def correct_user?
+        puts "---current user: #{@user.id} link user id: #{params[:user_id]}"
+        current_id = @user.id 
+        link_id = params[:user_id]
+        if current_id.eql?(link_id)
+            puts "---current id: #{current_id} link id: #{link_id}"
+            redirect_to controller: 'users', action: 'show', id: "#{params[:user_id]}"
+        end
+    end
 
 	def find_favorite_clip 
 		@favorite_clip  = FavoriteClip.find(params[:id])
